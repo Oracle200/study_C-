ef93b8158db9bc38a940e8789c230f3d0b1175df
Ilnur <sal73ilnur@gmail.com>
2021-08-02 15:29:08 +0400

    создал папку и первый проект

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Привет,\nя Василий!");
+        }       
+    }
+}
