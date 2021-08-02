7e814985545b201985c4bd8a368ec4cd60f21ba2
Ilnur <sal73ilnur@gmail.com>
2021-08-02 18:51:00 +0400

    Сделал задания 4 урока

--- Math/Program.cs
+++ Math/Program.cs
@@ -4,9 +4,32 @@ namespace Math
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            /*Задание 1
+            int a, b, res;
+            Console.Write("a = ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b = ");
+            b = Convert.ToInt32(Console.ReadLine());
+            res = a % b;
+            Console.WriteLine("Result: {0}", res);
+            Console.ReadKey();*/
+
+            /*Задание2
+            const int a = 20;
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Сложение: "+ (a + b));
+            Console.WriteLine("Вычитание: "+ (a - b));
+            Console.WriteLine("Умножение: "+ (a * b));
+            Console.WriteLine("Деление : "+ (a / b));
+            Console.ReadKey();*/
+            
+            /*Заданеи 3*/
+            int a = -5;
+            a *= 7;
+            a--;
+            System.Console.WriteLine(a);
         }
     }
 }
