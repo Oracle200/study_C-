bdb23b7f7f52dc89274e277c31b71be1723ed0e3
Ilnur <sal73ilnur@gmail.com>
2021-08-03 14:57:26 +0400

    Сделал задания для 5 урока

--- IF_ELSE/Program.cs
+++ IF_ELSE/Program.cs
@@ -4,9 +4,37 @@ namespace IF_ELSE
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            /*Задание 1
+            int a =Convert.ToInt32(Console.ReadLine());
+            int b =Convert.ToInt32(Console.ReadLine());
+            if ( a%2==0 && b%2==0)
+                System.Console.WriteLine("Оба числа чётные");
+            else if (a%2==0)
+                System.Console.WriteLine("чётное только А");
+            else if (b%2==0)
+                System.Console.WriteLine("чётное только Б");
+            else
+                System.Console.WriteLine("нету чётных чисел");*/
+
+            
+            /*Задание 2
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int res = 0;
+            if (b==0)
+                res = 1;
+            else 
+                res = a / b;
+            System.Console.WriteLine(res); */
+
+            /*Задание 3 */
+            int a = Convert.ToInt32(Console.ReadLine());
+            if ((a < 50 && a >= 32 && a != 37) || (a == 0 || a == 15))
+                System.Console.WriteLine("Working");
+            else 
+                System.Console.WriteLine("NoWorking");   
         }
     }
 }
