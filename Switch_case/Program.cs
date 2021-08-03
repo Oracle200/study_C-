91463f9b6d17ce0dbeddf01858356c1a9c99184c
Ilnur <sal73ilnur@gmail.com>
2021-08-03 15:15:35 +0400

    Создал папку для 6 урока

---
+++ Switch_case/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
