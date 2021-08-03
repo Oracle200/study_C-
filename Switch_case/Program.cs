bfa1e44dfb75a1f0419ffb4dddae0b06d9937f3c
Ilnur <sal73ilnur@gmail.com>
2021-08-03 15:28:09 +0400

    Сделал задание по 6 уроку

--- Switch_case/Program.cs
+++ Switch_case/Program.cs
@@ -6,7 +6,45 @@ namespace Switch_case
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Задание 1
+            int num = 11;
+            switch(num)
+            {
+                case 10:
+                    System.Console.WriteLine("Number is 10");
+                    break;
+                case 15:
+                    System.Console.WriteLine("Number is 15");
+                    break;
+                default:
+                    System.Console.WriteLine("Something else");
+                    break;                                           
+            }                                                       */
+            
+            // Задание 2
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int res = 0;
+            char simvol = Convert.ToChar(Console.ReadLine());
+            switch(simvol)
+            {
+                case '+' :
+                    res = a+b;
+                    System.Console.WriteLine(res);
+                    break;
+                case '-' :
+                    res = a-b;
+                    System.Console.WriteLine(res);                
+                    break;
+                case '*' :
+                    res = a*b;
+                    System.Console.WriteLine(res);                
+                    break;
+                case '/' :
+                    res = a/b;
+                    System.Console.WriteLine(res);                
+                    break;                    
+            }
         }
     }
 }
