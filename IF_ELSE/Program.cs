53ef0487c6cd2a7e830d627ed0737b1cb51d94dd
Ilnur <sal73ilnur@gmail.com>
2021-08-03 14:33:53 +0400

    Создал папку для 5 урока

---
+++ IF_ELSE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IF_ELSE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
