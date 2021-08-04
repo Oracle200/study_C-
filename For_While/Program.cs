2d9c7dbd785079f55521d3c8f9523a1b8849b346
Ilnur <sal73ilnur@gmail.com>
2021-08-04 15:53:44 +0400

    Сделал задание по 7 уроку

--- For_While/Program.cs
+++ For_While/Program.cs
@@ -4,9 +4,46 @@ namespace For_While
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            /*Задание 1
+            for (int i = 25; i<=29; i++)
+            {
+                if (i == 27)
+                    continue;
+                System.Console.WriteLine(i);
+            }                                    */
+            
+            /*Задание 2
+            const int a = 13;
+            int flag = 0;
+            while(a != flag)
+            {
+                System.Console.WriteLine("Ведите число 13: ");
+                flag = Convert.ToInt32(Console.ReadLine());
+                if (a == flag)
+                {
+                    System.Console.WriteLine("You win");
+                    break;
+                }
+            }                                                  */
+
+            /*Задание 3
+            for (int i = -30; i <= 0; i+=3)
+            {
+                if (i == -27 || i == -21 || i == -15)
+                    continue;
+                System.Console.WriteLine(i); 
+            }                                  */
+
+            /*Задание 4 */
+            int a = 1;
+            while (a != 0)
+            {
+                a = Convert.ToInt32(Console.ReadLine());
+                if (a == 1)
+                    continue;
+            }
         }
     }
 }
