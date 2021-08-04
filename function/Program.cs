8684cf6086faa3fe57a24cc9a743c112d88a5c26
Ilnur <sal73ilnur@gmail.com>
2021-08-04 19:11:17 +0400

    Создал папку для 10 урока

---
+++ function/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
