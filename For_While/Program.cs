893316d6a5350f3f8ad7dc06d6c526449a14524c
Ilnur <sal73ilnur@gmail.com>
2021-08-04 14:35:43 +0400

    Создал папку для 7 урока

---
+++ For_While/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace For_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
